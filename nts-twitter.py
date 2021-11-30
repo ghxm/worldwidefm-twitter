89e33b9752bc608cbc101b4351c7df9ea83f9ece
ghxm <max.haag@gmx.net>
2021-11-30 13:53:48 +0100

    some updates again

--- nts-twitter.py
+++ nts-twitter.py
@@ -21,7 +21,7 @@ def main():
     parser.add_argument("-n", "--numshows", help="Number of shows to check", default=25,
                     type=int)
     parser.add_argument("-t", "--text", help="Tweet text. Use {tw_name} and {url} to include upload name and url",
-                        default = "{tw_name} (broadcasted {date}, {location}) was just uploaded to nts.live. Give it a listen for some {genres}. {url}",
+                        default = "{tw_name} (broadcasted {date}, {location}) was uploaded to nts.live. Give it a listen for some {genres}. {url}",
                     type=str)
     parser.add_argument("-d", "--dry", help="Dry run (don't tweet)", action="store_true", default=False)
     args = parser.parse_args()
