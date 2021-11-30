b111e81be303b951e76b2d718a0ab6f954f78f09
Maximilian Haag <max.haag@gmx.net>
2021-11-30 09:59:22 +0100

    Initial commit

---
+++ nts.py
@@ -0,0 +1,8 @@
+hosts = {
+    'AD 93': 'AD93rec',
+    'Flo': 'flollid',
+    'SKY H1': 'SKY_H1_',
+    'Tasker': 'NicTasker',
+    'Death Is Not The End': 'deathisnot',
+    'Zakia': 'zakiasewell'
+}
