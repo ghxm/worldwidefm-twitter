53d6098679e55e7f312c22528846f280a8e22998
ghxm <max.haag@gmx.net>
2021-12-15 17:50:36 +0100

    add hosts

--- worldwidefm.py
+++ worldwidefm.py
@@ -1,1 +1,6 @@
-hosts = {'Gilles Peterson|Gilles': 'gillespeterson'}
+hosts = {'Gilles Peterson|Gilles': 'gillespeterson',
+         'DJ Deep': 'cyrildeep',
+         'DJ Ron Trent': 'DJRONTRENT',
+         'Papaoul': 'papaoul',
+         'Tomorrow(?:.){1}s Warriors': 'Tom_Warriors',
+         'Viva Msimang': 'VivaMsimang'}
