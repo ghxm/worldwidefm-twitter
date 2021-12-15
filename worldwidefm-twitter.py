3fe300cd09e6c1a3f86dbb1eb17fe6e2b5d42cf7
ghxm <max.haag@gmx.net>
2021-12-15 16:42:07 +0100

    adapt for worldwidefm

--- worldwidefm-twitter.py
+++ worldwidefm-twitter.py
@@ -5,11 +5,15 @@ import twitter_auth
 import sqlite3
 import re
 import nts
+import worldwidefm
 
 
 def add_host_twitter(show_name):
 
-    for name, user in nts.hosts.items():
+    hosts = nts.hosts
+    hosts.update(worldwidefm.hosts)
+
+    for name, user in hosts.items():
         show_name = re.sub(fr'(?:\b|^)({name})(?:\b|$)', f'\g<1> (@{user})', show_name, count=1, flags=re.IGNORECASE|re.MULTILINE)
 
     return show_name
@@ -21,7 +25,7 @@ def main():
     parser.add_argument("-n", "--numshows", help="Number of shows to check", default=25,
                     type=int)
     parser.add_argument("-t", "--text", help="Tweet text. Use {tw_name} and {url} to include upload name and url",
-                        default = "{tw_name} ({date}, {location}) was uploaded to nts.live. Give it a listen for some {genres}. {url}",
+                        default = "{tw_name} ({date}) was uploaded to worldwidefm.net. Give it a listen for some {genres}. {url}",
                     type=str)
     parser.add_argument("-d", "--dry", help="Dry run (don't tweet)", action="store_true", default=False)
     args = parser.parse_args()
@@ -33,7 +37,7 @@ def main():
     client = tweepy.Client(bearer_token=twitter_auth.bearer_token, consumer_key=twitter_auth.consumer_key, consumer_secret=twitter_auth.consumer_secret,
                         access_token=twitter_auth.access_token, access_token_secret=twitter_auth.access_token_secret, wait_on_rate_limit=True)
 
-    con = sqlite3.connect('nts.sqlite3', isolation_level=None)
+    con = sqlite3.connect('worldwidefm.sqlite3', isolation_level=None)
     cur = con.cursor()
 
     while n_shows <= args.numshows:
@@ -43,13 +47,14 @@ def main():
 
         print('Fetching shows')
 
-        url = f'https://www.nts.live/api/v2/search/episodes?offset={offset}&limit=12'
+        url = f'https://worldwidefm.net/cached_api'
 
-        feed = requests.get(url)
+        payload = {"operationName":"getEntries","variables":{"offset":offset,"limit":50,"section":"episode","player":["not",""]},"query":"query getEntries($section: [String], $offset: Int, $limit: Int, $player: [QueryArgument]) {\n  entries(\n    section: $section\n    limit: $limit\n    offset: $offset\n    player: $player\n    orderBy: \"broadcastDate DESC, postDate DESC\"\n  ) {\n    id\n    uri\n    title\n    postDate @formatDateTime(format: \"d.m.y\")\n    ...ScheduleDay\n    ...Offers\n    ...Editorial\n    ...Episode\n    __typename\n  }\n}\n\nfragment ScheduleDay on scheduleDay_scheduleDay_Entry {\n  scheduleDate @formatDateTime(format: \"yy-m-d\", timezone: \"Europe/London\")\n  scheduleContent {\n    id\n    __typename\n    ... on scheduleContent_scheduleDayItems_BlockType {\n      startTime @formatDateTime(format: \"GG:i\", timezone: \"Europe/London\")\n      endTime @formatDateTime(format: \"GG:i\", timezone: \"Europe/London\")\n      altTitle\n      episode {\n        title\n        uri\n        __typename\n      }\n      __typename\n    }\n  }\n  __typename\n}\n\nfragment Offers on offers_offers_Entry {\n  offerCode\n  offerDate\n  offerLink\n  offerState\n  offerText\n  offerType\n  postDate @formatDateTime(format: \"d.m.y\")\n  __typename\n}\n\nfragment Editorial on editorial_editorial_Entry {\n  id\n  title\n  description\n  thumbnail {\n    url @transform(width: 1200, height: 1200, immediately: true)\n    __typename\n  }\n  postDate @formatDateTime(format: \"d.m.y\")\n  __typename\n}\n\nfragment Episode on episode_episode_Entry {\n  id\n  title\n  description\n  thumbnail {\n    url @transform(width: 1200, height: 1200, immediately: true)\n    __typename\n  }\n  genreTags {\n    title\n    slug\n    __typename\n  }\n  broadcastDate @formatDateTime(format: \"d.m.y\")\n  episodeCollection {\n    id\n    title\n    uri\n    __typename\n  }\n  __typename\n}\n"}
+        feed = requests.post(url, json=payload)
 
         feed_dict = feed.json()
 
-        uploads = [{'name': r.get('title'), 'tw_name': add_host_twitter(r.get('title')), 'slug': r.get('article', {}).get('path'), 'url': 'https://www.nts.live' + r.get('article').get('path'), 'date': r.get('local_date'), 'location': r.get('location'), 'genres': [g['name'] for g in r.get('genres')]}  for r in feed_dict['results']]
+        uploads = [{'name': r.get('title'), 'tw_name': add_host_twitter(r.get('title')), 'slug': r.get('uri'), 'url': 'https://www.worldwidefm.net' + r.get('uri'), 'date': r.get('postDate'), 'location': r.get('location', ''), 'genres': [g['title'] for g in r.get('genreTags')]}  for r in feed_dict['data']['entries']]
 
         print(str(len(uploads)) +  ' found')
 
---
+++ worldwidefm.py
@@ -0,0 +1,1 @@
+hosts = {'Gilles Peterson|Gilles': 'gillespeterson'}
