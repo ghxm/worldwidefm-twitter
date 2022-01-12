2b1b0378d9a0bd1186707aa7346f1949c5ddaf7f
ghxm <max.haag@gmx.net>
2022-01-12 15:22:19 +0100

    add fka mash

--- worldwidefm.py
+++ worldwidefm.py
@@ -2,6 +2,7 @@ hosts = {'Gilles Peterson|Gilles': 'gillespeterson',
          'Ambient Flo|Auntie Flo': 'Auntie_Flo',
          'DJ Deep': 'cyrildeep',
          'DJ Ron Trent': 'DJRONTRENT',
+         'Fka Mash': 'RealFkaMash',
          'Papaoul': 'papaoul',
          'Tomorrow(?:.){1}s Warriors': 'Tom_Warriors',
          'Viva Msimang': 'VivaMsimang'}
